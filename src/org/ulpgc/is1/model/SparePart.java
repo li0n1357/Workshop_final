55b4666f1b3edea3657debf385c38c30c0558f8a
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 22:26:29 +0100

    Clases SparePart y Vehicle modificadas y completas.

--- src/org/ulpgc/is1/model/SparePart.java
+++ src/org/ulpgc/is1/model/SparePart.java
@@ -1,12 +1,23 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SparePart {
     private final String name;
     private final int price;
 
-    public SparePart(String name, int price) {
+    public SparePart(String name, int price, RepairManager repairManager) {
         this.name = name;
         this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPrice() {
+        return this.price;
     }
 
     @Override
--- src/org/ulpgc/is1/model/Vehicle.java
+++ src/org/ulpgc/is1/model/Vehicle.java
@@ -1,14 +1,52 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vehicle {
     private final String make;
     private final String model;
     private final Plate plate;
+    private final Customer owner;
+    private final RepairManager repairManager;
+    private final List<Repair> repairs;
 
-    public Vehicle(String make, String model, Plate plate) {
+
+    public Vehicle(String make, String model, Plate plate, Customer owner, RepairManager repairManager) {
         this.make = make;
         this.model = model;
         this.plate = plate;
+        this.owner = owner;
+        this.repairManager = repairManager;
+        this.repairs = new ArrayList<>();
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Plate getPlate() {
+        return plate;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public RepairManager getRepairManager() {
+        return repairManager;
+    }
+
+    public void addRepair(Repair repair) {
+        this.repairs.add(repair);
+    }
+
+    public List<Repair> getRepairs() {
+        return this.repairs;
     }
 
     @Override
