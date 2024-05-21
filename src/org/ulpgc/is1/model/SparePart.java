d97afafe1d00bd19e64b3b970ade8a8d34057a3d
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 17:24:31 +0100

    Esquema de Vehicle hecho.

--- src/org/ulpgc/is1/model/SparePart.java
+++ src/org/ulpgc/is1/model/SparePart.java
@@ -1,2 +1,16 @@
-package org.ulpgc.is1.model;public class SparePart {
+package org.ulpgc.is1.model;
+
+public class SparePart {
+    private final String name;
+    private final int price;
+
+    public SparePart(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + this.name + ". Precio: " + this.price;
+    }
 }
