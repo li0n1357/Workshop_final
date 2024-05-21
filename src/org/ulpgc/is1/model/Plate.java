eedb70fb47cb72b569bcc335309362e0d0d479b9
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 17:29:10 +0100

    Clase (récord) Plate completa.

--- src/org/ulpgc/is1/model/Plate.java
+++ src/org/ulpgc/is1/model/Plate.java
@@ -1,2 +1,19 @@
-package org.ulpgc.is1.model;public class Plate {
+package org.ulpgc.is1.model;
+
+public record Plate(String number) {
+
+    public String isValid() {
+        if (number == null) {
+            throw new IllegalArgumentException("La placa debe tener 6 caracteres alfanuméricos.");
+        }
+        if (!number.matches("[A-Za-z0-9]{6}")) {
+            throw new IllegalArgumentException("La placa debe tener 6 caracteres alfanuméricos.");
+        } else {
+            return this.number;
+        }
+    }
+
+    public String toString() {
+        return this.number;
+    }
 }
