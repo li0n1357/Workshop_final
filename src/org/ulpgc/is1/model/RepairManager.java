0b12e10b6ba4b7997022a4fd99ecd6f14d114e6d
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 18:16:01 +0100

    Clase RepairManager hecha.

--- src/org/ulpgc/is1/model/RepairManager.java
+++ src/org/ulpgc/is1/model/RepairManager.java
@@ -1,2 +1,34 @@
-package org.ulpgc.is1.model;public class RepairManager {
+package org.ulpgc.is1.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepairManager {
+    private final Map<String, Mechanic> mechanics;
+    private final Map<String, Vehicle> vehicles;
+
+    public RepairManager() {
+        this.mechanics = new HashMap<>();
+        this.vehicles = new HashMap<>();
+    }
+
+    public void addMechanic(String name, Mechanic mechanic) {
+        mechanics.put(name, mechanic);
+    }
+
+    public void addVehicle(String model, Vehicle vehicle) {
+        vehicles.put(model, vehicle);
+    }
+
+    public Mechanic getMechanic(String name) {
+        return mechanics.get(name);
+    }
+
+    public Vehicle getVehicle(String model) {
+        return vehicles.get(model);
+    }
+
+    public void repair() {
+
+    }
 }
