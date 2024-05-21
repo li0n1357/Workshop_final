3dce235d35302e3a1ac1127333c2f90afbf4ae89
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 17:36:21 +0100

    Clase Customer completa.

--- src/org/ulpgc/is1/model/Customer.java
+++ src/org/ulpgc/is1/model/Customer.java
@@ -1,2 +1,24 @@
-package org.ulpgc.is1.model;public class Customer {
+package org.ulpgc.is1.model;
+
+public class Customer {
+    private final String name;
+    private final Phone phone;
+
+    public Customer(String name, Phone phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Phone getPhone() {
+        return this.phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente: " + this.name + ". Teléfono de contacto: " + this.phone;
+    }
 }
