453bd86751df122ebe3839ab3a2271c06f9ce1e2
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 17:31:52 +0100

    Clase Item completa.

--- src/org/ulpgc/is1/model/Item.java
+++ src/org/ulpgc/is1/model/Item.java
@@ -1,2 +1,18 @@
-package org.ulpgc.is1.model;public class Item {
+package org.ulpgc.is1.model;
+
+public class Item {
+    private final int quantity;
+
+    public Item(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Cantidad: " + this.quantity;
+    }
 }
