e048c9bd9b83f30168e3ea50404d959c87bc7bea
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 17:04:47 +0100

    Clase (récord) Phone completa.

--- src/org/ulpgc/is1/model/Phone.java
+++ src/org/ulpgc/is1/model/Phone.java
@@ -1,2 +1,12 @@
-package org.ulpgc.is1.model;public class Phone {
+package org.ulpgc.is1.model;
+
+public record Phone(String number) {
+
+    public String isValid() {
+        if (!number.matches("\\d{9}")) {
+            throw new IllegalArgumentException("El número de teléfono debe tener 9 digitos.");
+        } else {
+            return this.number;
+        }
+    }
 }
