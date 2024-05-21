578cce1ad1bae8332fe9c968836c913931685cdd
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 20:15:12 +0100

    Clases Mechanic y Repair modificadas y completas.

--- src/org/ulpgc/is1/model/Mechanic.java
+++ src/org/ulpgc/is1/model/Mechanic.java
@@ -1,12 +1,19 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mechanic {
     private final String name;
     private final String surname;
+    private final List<Repair> repairs;
+    private final RepairManager repairManager;
 
-    public Mechanic(String name, String surname) {
+    public Mechanic(String name, String surname, RepairManager repairManager) {
         this.name = name;
         this.surname = surname;
+        this.repairs = new ArrayList<>();
+        this.repairManager = repairManager;
     }
 
     public String getName() {
@@ -15,6 +22,14 @@ public class Mechanic {
 
     public String getSurname() {
         return this.surname;
+    }
+
+    public void addRepair(Repair repair) {
+        this.repairs.add(repair);
+    }
+
+    public RepairManager getRepairManager() {
+        return this.repairManager;
     }
 
     @Override
--- src/org/ulpgc/is1/model/Repair.java
+++ src/org/ulpgc/is1/model/Repair.java
@@ -1,6 +1,8 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Repair {
     private static int NEXT_ID = 0;
@@ -9,13 +11,19 @@ public class Repair {
     private final String description;
     private final int effort;
     private final Payment payment;
+    private final BreakdownTypes type;
+    private final List<SparePart> partsReplaced;
+    private final Vehicle vehicle;
 
-    public Repair(Date date, String description, int effort, Payment payment) {
+    public Repair(Date date, String description, int effort, Payment payment, BreakdownTypes type, List<SparePart> partsReplaced, Vehicle vehicle) {
         this.date = new Date(date.getTime());
         this.id = NEXT_ID++;
         this.description = description;
         this.effort = effort;
         this.payment = payment;
+        this.type = type;
+        this.partsReplaced = new ArrayList<>();
+        this.vehicle = vehicle;
     }
 
     public Date getDate() {
@@ -34,15 +42,32 @@ public class Repair {
         return this.effort;
     }
 
-    //getPayment() == price()
     public Payment getPayment() {
         return this.payment;
     }
 
+    public BreakdownTypes getType() {
+        return this.type;
+    }
+
+    public List<SparePart> getPartsReplaced() {
+        return partsReplaced;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
     @Override
     public String toString() {
-        return "Reparaciones -> " + "id: " + this.id + ", fecha: '" + this.date + '\'' +
-                ", pago realizado: '" + payment.amount() + ", descripción: '" + this.description +
-                ", effort: " + this.effort;
+        return "Reparaciones -> " +
+                "id: " + id +
+                ", fecha: '" + date + '\'' +
+                ", descripción: '" + description + '\'' +
+                ", effort: " + effort +
+                ", pago realizado: '" + payment.amount() + '\'' +
+                ", tipo de reparación: '" + type + '\'' +
+                ", partes reemplazadas: " + partsReplaced +
+                ", vehículo reparado: " + vehicle;
     }
 }
