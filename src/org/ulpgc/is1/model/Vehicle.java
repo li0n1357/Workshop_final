a2796d25fd126a6023c0f0c3c8dd3b4bad143d11
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 17:21:44 +0100

    Esquema de Vehicle hecho.

--- src/org/ulpgc/is1/model/Vehicle.java
+++ src/org/ulpgc/is1/model/Vehicle.java
@@ -1,2 +1,18 @@
-package org.ulpgc.is1.model;public class Vehicle {
+package org.ulpgc.is1.model;
+
+public class Vehicle {
+    private final String make;
+    private final String model;
+    private final Plate plate;
+
+    public Vehicle(String make, String model, Plate plate) {
+        this.make = make;
+        this.model = model;
+        this.plate = plate;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo -> " + "marca: '" + make + ", modelo: '" + model + ", matrícula: " + plate;
+    }
 }
