360c6789ef73cd4b60748d25a7ed4f8b079ab1c0
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 22:27:33 +0100

    Clase RepairManager completa.

--- src/org/ulpgc/is1/model/RepairManager.java
+++ src/org/ulpgc/is1/model/RepairManager.java
@@ -2,14 +2,20 @@ package org.ulpgc.is1.model;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RepairManager {
     private final Map<String, Mechanic> mechanics;
     private final Map<String, Vehicle> vehicles;
+    private final Map<Integer, Repair> repairs;
+    private final List<SparePart> parts;
 
     public RepairManager() {
         this.mechanics = new HashMap<>();
         this.vehicles = new HashMap<>();
+        this.repairs = new HashMap<>();
+        this.parts = new ArrayList<>();
     }
 
     public void addMechanic(String name, Mechanic mechanic) {
@@ -20,6 +26,10 @@ public class RepairManager {
         vehicles.put(model, vehicle);
     }
 
+    public void addPart(SparePart part) {
+        parts.add(part);
+    }
+
     public Mechanic getMechanic(String name) {
         return mechanics.get(name);
     }
@@ -28,7 +38,34 @@ public class RepairManager {
         return vehicles.get(model);
     }
 
-    public void repair() {
+    public void addRepair(Repair repair) {
+        repairs.put(repair.getID(), repair);
+        Vehicle vehicle = repair.getVehicle();
+        if (vehicle != null) {
+            vehicle.addRepair(repair);
+        }
+    }
 
+    public void removeVehicle(String model) {
+        vehicles.remove(model);
+    }
+
+    public int getNumberOfVehicles() {
+        return vehicles.size();
+    }
+
+    public Map<Integer, Repair> getRepairs() {
+        return repairs;
+    }
+
+    public void repair(Repair repair) {
+        Vehicle vehicle = repair.getVehicle();
+        if (vehicle != null) {
+            addRepair(repair);
+            Payment payment = repair.getPayment();
+            System.out.println("Se ha facturado $" + payment.amount() + " por la reparación.");
+        } else {
+            System.out.println("No se pudo realizar la reparación. Vehículo no encontrado.");
+        }
     }
 }
