efe9c12afd41625ecf0c96661823d5b53de2b25a
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 17:10:09 +0100

    Clase (récord) Payment completa.

--- src/org/ulpgc/is1/model/Payment.java
+++ src/org/ulpgc/is1/model/Payment.java
@@ -1,2 +1,15 @@
-package org.ulpgc.is1.model;public class Payment {
+package org.ulpgc.is1.model;
+
+import java.util.Date;
+
+public record Payment(Date date, int amount) {
+    public Payment(Date date, int amount) {
+        this.date = new Date(date.getTime());
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Importe: " + this.amount + ". Fecha: " + this.date;
+    }
 }
