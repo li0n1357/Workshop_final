adc91504e8751d9347a28076452002f9f287b14f
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-21 17:44:18 +0100

    Clase Mechanic parcialmente completa.

--- src/org/ulpgc/is1/model/Mechanic.java
+++ src/org/ulpgc/is1/model/Mechanic.java
@@ -1,2 +1,24 @@
-package org.ulpgc.is1.model;public class Mechanic {
+package org.ulpgc.is1.model;
+
+public class Mechanic {
+    private final String name;
+    private final String surname;
+
+    public Mechanic(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre y Apellido: " + this.name + " " + this.surname;
+    }
 }
