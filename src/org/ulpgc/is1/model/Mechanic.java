a1700b4b19b256d4b06b3dfd9515ea796d8d675b
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-05-22 19:20:58 +0100

    Cambio para poner Nombre y Apellidos.

--- src/org/ulpgc/is1/model/Mechanic.java
+++ src/org/ulpgc/is1/model/Mechanic.java
@@ -34,6 +34,6 @@ public class Mechanic {
 
     @Override
     public String toString() {
-        return "Nombre y Apellido: " + this.name + " " + this.surname;
+        return "Nombre y Apellidos: " + this.name + " " + this.surname;
     }
 }
